a9e59638a1d73737c524458047daf316d9a8ca18
thoucsin <1598244350@qq.com>
2018-10-20 13:19:17 +0800

    add homework6

---
+++ homework6/program1/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace program1
+{
+    class MyException : Exception
+    {
+        private string id;
+        public MyException(string ex)
+        {
+            id = ex;
+        }
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+    }
+    [Serializable]
+    public class Order
+    {
+        //private int ID;
+        //private string username;
+        public List<OrderDetails> details;
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public Order()
+        {
+            this.ID = 0;
+            this.Username = "";
+            details = new List<OrderDetails>();
+        }
+        public Order(int ID, string username)
+        {
+            this.ID = ID;
+            this.Username = username;
+            details = new List<OrderDetails>();
+        }
+        //public override string ToString()
+        //{
+        //    string ret = "";
+        //    ret = ret+("订单号:" + getID()+"/n");
+        //    ret = ret + ("客户名:" + getUsername() + "/n");
+        //    for(int i = 0; i<details.Count-1;i++)
+        //    {
+        //        ret = ret + ("商品名:" + details[i].getName() + " ");
+        //        ret = ret + ("单价:" + details[i].getPrice() + " ");
+        //        ret = ret + ("数量:" + details[i].getAccount() + "/n");
+        //    }
+        //    return ret;
+        //}
+        public int addUp()
+        {
+            int ret = 0;
+            for (int i = 0; i < details.Count; i++)
+            {
+                ret += details[i].getPrice() * details[i].getAccount();
+            }
+            return ret;
+        }
+        public void addDetail(OrderDetails od)
+        {
+            details.Add(od);
+        }
+        public void delDetail(string name)
+        {
+            try
+            {
+                bool done = false;
+                for (int i = details.Count - 1; i >= 0; i--)
+                {
+                    if (name == details[i].getName())
+                    {
+                        details.RemoveAt(i);
+                        done = true;
+                    }
+                }
+                if (!done)
+                {
+                    throw new MyException("没有名为" + name + "的商品");
+                }
+            }
+            catch (MyException e)
+            {
+                Console.Write(e.Id);
+            }
+        }
+        public int getID()
+        {
+            return ID;
+        }
+        public string getUsername()
+        {
+            return Username;
+        }
+    }
+    public class OrderDetails
+    {
+        //private string name;
+        //private int price;
+        //private int account;
+        public string Name { set; get; }
+        public int Price { set; get; }
+        public int Account { set; get; }
+        public OrderDetails(string name, int price, int account)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Account = account;
+        }
+        public OrderDetails()
+        {
+            this.Name = "";
+            this.Price = 0;
+            this.Account = 0;
+        }
+        public int getAccount()
+        {
+            return Account;
+        }
+        public void setName(string name)
+        {
+            this.Name = name;
+        }
+        public void setAccount(int account)
+        {
+            this.Account = account;
+        }
+        public void setPrice(int price)
+        {
+            this.Price = price;
+        }
+        public int getPrice()
+        {
+            return Price;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+
+    }
+    public class OrderService
+    {
+        public List<Order> orders = null;
+        public OrderService()
+        {
+            orders = new List<Order>();
+        }
+        public void addOrder(Order order)
+        {
+            orders.Add(order);
+        }
+        public void delOrder(int ID)
+        {
+            try
+            {
+                bool done = false;
+                for (int i = orders.Count - 1; i >= 0; i--)
+                {
+                    if (orders[i].getID() == ID)
+                    {
+                        orders.RemoveAt(i);
+                        done = true;
+                        break;
+                    }
+                }
+                if (!done)
+                {
+                    throw new MyException("没有ID为" + ID + "的订单");
+                }
+            }
+            catch (MyException e)
+            {
+                Console.Write(e.Id);
+            }
+        }
+        public List<Order> checkOrder(int id)
+        {
+            var m = from o in orders where o.getID() == id select o;
+            return m.ToList();
+        }
+        public List<Order> checkOrder(string username)
+        {
+            var m = from o in orders where o.getUsername() == username select o;
+            return m.ToList();
+        }
+        public bool Check(Order o, string s)
+        {
+            bool ok = false;
+            for (int i = 0; i < o.details.Count; i++)
+            {
+                if (o.details[i].getName() == s)
+                {
+                    ok = true;
+                    break;
+                }
+            }
+            return ok;
+        }
+        public List<Order> checkOrder(string name, int k)
+        {
+            var m = from o in orders where Check(o, name) select o;
+            return m.ToList();
+        }
+        public void Export(string xmlFileName)
+        {
+            XmlSerializer xmlser = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(xmlFileName, FileMode.Create))
+            {
+                xmlser.Serialize(fs, orders);
+            }
+        }
+
+        public List<Order> Import(string xmlFileName)
+        {
+            List<Order> ret = null;
+            XmlSerializer xmlser = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(xmlFileName, FileMode.Open))
+            {
+                ret = (List<Order>)xmlser.Deserialize(fs);
+            }
+            return ret;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService os = new OrderService();
+            for (int i = 0; i < 10; i++)
+            {
+                Order order = new Order(i, "A" + i.ToString());
+                for (int j = 0; j < 3; j++)
+                {
+                    order.details.Add(new OrderDetails("B" + j.ToString(), 3 * j, j));
+                }
+                os.orders.Add(order);
+            }
+            Order order1 = new Order(1222, "王有钱");
+            order1.details.Add(new OrderDetails("ex", 5000, 3));
+            os.orders.Add(order1);
+            var expensive = from o in os.orders where o.addUp() > 10000 select o;
+            foreach (Order o in expensive)
+            {
+                Console.WriteLine("价格超过一万元的订单如下:");
+                Console.WriteLine("订单号:" + o.getID());
+                Console.WriteLine("客户名:" + o.getUsername());
+                for (int i = 0; i < o.details.Count; i++)
+                {
+                    Console.WriteLine("商品:" + o.details[i].getName() + " 单价:" + o.details[i].getPrice()
+                        + " 数量:" + o.details[i].getAccount());
+                }
+            }
+            os.Export("orders.xml");
+        }
+    }
+}
---
+++ homework6/program1Tests/OrderServiceTests.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using program1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace program1.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void OrderServiceTest()
+        {
+            OrderService os = new OrderService();
+            Assert.IsNotNull(os.orders);
+        }
+
+        [TestMethod()]
+        public void addOrderTest()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order();
+            os.addOrder(o);
+            Assert.IsTrue(os.orders.Count == 1);
+        }
+
+        [TestMethod()]
+        public void delOrderTest()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order();
+            os.addOrder(o);
+            int a1 = os.orders.Count;
+            os.delOrder(0);
+            int a2 = os.orders.Count;
+            Assert.IsTrue(a1 == 1 && a2 == 0);
+        }       
+        [TestMethod()]
+        public void checkOrderTest()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order(10, "aaa");
+            os.addOrder(o);
+            List<Order> ors = os.checkOrder(10);
+            Assert.IsTrue(ors.Count==1&&ors[0].getID()==10&& ors[0].getUsername()=="aaa");
+        }
+
+        [TestMethod()]
+        public void checkOrderTest1()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order(10, "aaa");
+            os.addOrder(o);
+            List<Order> ors = os.checkOrder("aaa");
+            Assert.IsTrue(ors.Count == 1 && ors[0].getID() == 10 && ors[0].getUsername() == "aaa");
+        }
+
+        [TestMethod()]
+        public void CheckTest()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order(10, "aaa");
+            OrderDetails od = new OrderDetails("a1", 0, 0);
+            o.addDetail(od);
+            bool b1 = os.Check(o, "a1");
+            bool b2 = os.Check(o, "b1");
+            Assert.IsTrue(b1 && !b2);
+        }
+
+        [TestMethod()]
+        public void checkOrderTest2()
+        {
+            OrderService os = new OrderService();
+            Order o1 = new Order(10, "aaa");
+            Order o2 = new Order(11, "abc");
+            OrderDetails od1 = new OrderDetails("a1", 2, 2);
+            OrderDetails od2 = new OrderDetails("a3", 2, 2);
+            o2.addDetail(od2);
+            o1.addDetail(od1);
+            os.addOrder(o1);
+            os.addOrder(o2);
+            List<Order> ors = os.checkOrder("a1", 0) ;
+            Assert.IsTrue(ors.Count==1&&ors[0].details[0].getName()=="a1");
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order(10, "aaa");
+            OrderDetails od = new OrderDetails("a1", 2, 3);
+            o.addDetail(od);
+            os.addOrder(o);
+            os.Export("test1.xml");
+            string s1, s2;
+            using (FileStream fr = new FileStream("test1.xml", FileMode.Open,FileAccess.Read))
+            {
+                s1 = File.ReadAllText("test1.xml", Encoding.UTF8);
+                s2 = "<?xml version=\"1.0\"?>\r\n"+
+                    "<ArrayOfOrder xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  "+
+                    "<Order>\r\n    <details>\r\n      <OrderDetails>\r\n        <Name>a1</Name>\r\n        "+
+                    "<Price>2</Price>\r\n        <Account>3</Account>\r\n      </OrderDetails>\r\n    "+
+                    "</details>\r\n    <ID>10</ID>\r\n    <Username>aaa</Username>\r\n  </Order>\r\n</ArrayOfOrder>";
+            }
+            Assert.IsTrue(s1==s2);
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            OrderService os = new OrderService();
+            Order o = new Order(10, "aaa");
+            OrderDetails od = new OrderDetails("a1",2,3);
+            o.addDetail(od);
+            os.addOrder(o);
+            os.Export("test2.xml");
+            List<Order> aim = os.Import("test2.xml");
+            bool b1 = (aim.Count == 1 && aim[0].details.Count == 1);
+            bool b2 = false;
+            if (b1)
+            {
+                if (aim[0].getID() == 10 && aim[0].getUsername() == "aaa")
+                {
+                    if (aim[0].details[0].getName() == "a1" && aim[0].details[0].getPrice() == 2
+                        && aim[0].details[0].getAccount() == 3) b2 = true;
+                }
+            }
+            Assert.IsTrue(b1&&b2);
+        }
+    }
+}

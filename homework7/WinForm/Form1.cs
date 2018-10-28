e1f4d219947db6ddaecb845785c074af55838754
thoucsin <1598244350@qq.com>
2018-10-28 10:43:02 +0800

    add homework7

---
+++ homework7/WinForm/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using program1;
+
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        OrderService os = new OrderService();
+        List<Order> ol = new List<Order>();
+        Order on = new Order(0, null);
+        Order o1 = new Order(1311, "aaaa");
+        Order o2 = new Order(1111, "asdas");
+        OrderDetails od1 = new OrderDetails("haha", 21, 2);
+        OrderDetails od2 = new OrderDetails("haha2", 21, 2);
+        OrderDetails od3 = new OrderDetails("haha3", 21, 2);
+        OrderDetails od4 = new OrderDetails("haha4", 21, 2);
+        bool checking = false;
+        public Form1()
+        {
+            o1.addDetail(od1);
+            o1.addDetail(od2);
+            o1.addDetail(od3);
+            o2.addDetail(od4);
+            os.addOrder(o1);
+            os.addOrder(o2);
+            InitializeComponent();
+            button6.Visible = false;
+            bindingSource1.DataSource = os.orders;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Order to = new Order(int.Parse(textBox1.Text), textBox2.Text);
+                os.addOrder(to);
+                textBox1.Text = null;
+                textBox2.Text = null;
+                bindingSource1.DataSource = ol;
+                bindingSource1.DataSource = os.orders;
+            }catch
+            {
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            os.orders.RemoveAt(i);
+            bindingSource1.DataSource = ol;
+            bindingSource1.DataSource = os.orders;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OrderDetails tod = new OrderDetails(textBox3.Text, int.Parse(textBox4.Text), int.Parse(textBox5.Text));
+                int i = dataGridView1.CurrentRow.Index;
+                os.orders[i].addDetail(tod);
+                textBox3.Text = null;
+                textBox4.Text = null;
+                textBox5.Text = null;
+                bindingSource1.DataSource = ol;
+                bindingSource1.DataSource = os.orders;
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            int j = dataGridView2.CurrentRow.Index;
+            os.orders[i].Details.RemoveAt(j);
+            bindingSource1.DataSource = ol;
+            bindingSource1.DataSource = os.orders;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = 0;
+                int tar = int.Parse(textBox6.Text);
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+                textBox5.Visible = false;
+                checking = true;
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                button6.Visible = true;
+                dataGridView1.ReadOnly = true;
+                dataGridView2.ReadOnly = true;
+                bindingSource1.DataSource = os.orders;
+                for(int i = 0; i < os.orders.Count;i++)
+                {
+                    if (os.orders[i].ID == tar) n++;
+                }
+                if (n > 0)
+                    bindingSource1.DataSource = os.orders.Where(o => o.ID == tar);
+                else
+                    bindingSource1.DataSource = on;
+            }            
+            catch
+            {
+               return;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!checking) return;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            textBox5.Visible = true;
+            checking = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button6.Visible = false;
+            dataGridView1.ReadOnly = false;
+            dataGridView2.ReadOnly = false;
+            textBox6.Text = null;
+            dataGridView2.Update();
+            bindingSource1.DataSource = os.orders;
+        }
+    }
+}
---
+++ homework7/program1/Program.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class MyException : Exception
+    {
+        private string id;
+        public MyException(string ex)
+        {
+            id = ex;
+        }
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+    }
+    public class Order
+    {
+        public List<OrderDetails> Details { get; set; }
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public Order(int ID, string username)
+        {
+            this.ID = ID;
+            this.Username = username;
+            Details = new List<OrderDetails>();
+        }
+        public int addUp()
+        {
+            int ret = 0;
+            for (int i = 0; i < Details.Count; i++)
+            {
+                ret += Details[i].Price * Details[i].Account;
+            }
+            return ret;
+        }
+        public void addDetail(OrderDetails od)
+        {
+            Details.Add(od);
+        }
+        public void delDetail(string name)
+        {
+            try
+            {
+                bool done = false;
+                for (int i = Details.Count - 1; i >= 0; i--)
+                {
+                    if (name == Details[i].Name)
+                    {
+                        Details.RemoveAt(i);
+                        done = true;
+                    }
+                }
+                if (!done)
+                {
+                    throw new MyException("没有名为" + name + "的商品");
+                }
+            }
+            catch (MyException e)
+            {
+                Console.Write(e.Id);
+            }
+        }
+    }
+    public class OrderDetails
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Account { get; set; }
+        public OrderDetails(string name, int price, int account)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Account = account;
+        }       
+    }
+    public class OrderService
+    {
+        //public List<Order> orders;
+        public List<Order> orders { get; set; }
+        public OrderService()
+        {
+            orders = new List<Order>();
+        }
+        public void addOrder(Order order)
+        {
+            orders.Add(order);
+        }
+        public void delOrder(int ID)
+        {
+            try
+            {
+                bool done = false;
+                for (int i = orders.Count - 1; i >= 0; i--)
+                {
+                    if (orders[i].ID == ID)
+                    {
+                        orders.RemoveAt(i);
+                        done = true;
+                        break;
+                    }
+                }
+                if (!done)
+                {
+                    throw new MyException("没有ID为" + ID + "的订单");
+                }
+            }
+            catch (MyException e)
+            {
+                Console.Write(e.Id);
+            }
+        }
+        public void changeOrder(int ID, string name1, string name2, int account, int price)
+        {
+            try
+            {
+                bool done = false;
+                for (int i = orders.Count - 1; i >= 0; i--)
+                {
+                    if (orders[i].ID == ID)
+                    {
+                        for (int j = 0; j < orders[i].Details.Count; j++)
+                        {
+                            if (orders[i].Details[j].Name == name1)
+                            {
+                                orders[i].Details[j].Name=name2;
+                                orders[i].Details[j].Price=price;
+                                orders[i].Details[j].Account=account;
+                                done = true;
+                            }
+                        }
+                    }
+                }
+                if (!done)
+                {
+                    throw new MyException("没有ID为" + ID + "的有" + name1 + "的订单");
+                }
+            }
+            catch (MyException e)
+            {
+                Console.Write(e.Id);
+            }
+        }
+        public void checkOrder(int id)
+        {
+            bool done = false;
+            var m = from o in orders where o.ID == id select o;
+            foreach (Order o in m)
+            {
+                done = true;
+                Console.WriteLine("订单号:" + o.ID);
+                Console.WriteLine("客户名:" + o.Username);
+                for (int i = 0; i < o.Details.Count; i++)
+                {
+                    Console.WriteLine("商品:" + o.Details[i].Name + " 单价:" + o.Details[i].Price
+                        + " 数量:" + o.Details[i].Account);
+                }
+            }
+            try
+            {
+                if (!done)
+                    throw new MyException("没有ID为" + id + "的订单");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine(e.Id);
+            }
+        }
+        public void checkOrder(string username)
+        {
+            bool done = false;
+            var m = from o in orders where o.Username== username select o;
+            foreach (Order o in m)
+            {
+                done = true;
+                Console.WriteLine("订单号:" + o.ID);
+                Console.WriteLine("客户名:" + o.Username);
+                for (int i = 0; i < o.Details.Count; i++)
+                {
+                    Console.WriteLine("商品:" + o.Details[i].Name + " 单价:" + o.Details[i].Price
+                        + " 数量:" + o.Details[i].Account);
+                }
+            }
+            try
+            {
+                if (!done)
+                    throw new MyException("没有客户为" + username + "的订单");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine(e.Id);
+            }
+        }
+        public bool Check(Order o, string s)
+        {
+            bool ok = false;
+            for (int i = 0; i < o.Details.Count; i++)
+            {
+                if (o.Details[i].Name == s)
+                {
+                    ok = true;
+                    break;
+                }
+            }
+            return ok;
+        }
+        public void checkOrder(string name, int k)
+        {
+            bool done = false;
+            //var m = orders.Where(o => Check(o, name));
+            var m = from o in orders where Check(o, name) select o;
+            foreach (Order o in m)
+            {
+                done = true;
+                Console.WriteLine("订单号:" + o.ID);
+                Console.WriteLine("客户名:" + o.Username);
+                for (int i = 0; i < o.Details.Count; i++)
+                {
+                    Console.WriteLine("商品:" + o.Details[i].Name + " 单价:" + o.Details[i].Price
+                        + " 数量:" + o.Details[i].Account);
+                }
+            }
+            try
+            {
+                if (!done)
+                    throw new MyException("没有购买了" + name + "的订单");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine(e.Id);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService os = new OrderService();
+            for (int i = 0; i < 10; i++)
+            {
+                Order order = new Order(i, "A" + i.ToString());
+                for (int j = 0; j < 3; j++)
+                {
+                    order.Details.Add(new OrderDetails("B" + j.ToString(), 3 * j, j));
+                }
+                os.orders.Add(order);
+            }
+            Order order1 = new Order(1222, "王有钱");
+            order1.Details.Add(new OrderDetails("ex", 5000, 3));
+            os.orders.Add(order1);
+            var expensive = from o in os.orders where o.addUp() > 10000 select o;
+            foreach (Order o in expensive)
+            {
+                Console.WriteLine("价格超过一万元的订单如下:");
+                Console.WriteLine("订单号:" + o.ID);
+                Console.WriteLine("客户名:" + o.Username);
+                for (int i = 0; i < o.Details.Count; i++)
+                {
+                    Console.WriteLine("商品:" + o.Details[i].Name + " 单价:" + o.Details[i].Price
+                        + " 数量:" + o.Details[i].Account);
+                }
+            }
+            os.checkOrder(50);
+            os.checkOrder("张三");
+            os.checkOrder("水", 0);
+            os.checkOrder(2);
+            os.checkOrder("A1");
+            os.checkOrder("b1", 0);
+        }
+    }
+}
